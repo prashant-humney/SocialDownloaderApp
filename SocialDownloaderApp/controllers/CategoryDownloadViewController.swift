87bc04d501e06a8d809a2fc4a90fac9fbdb82f3c
Prashant Humney <phumney@paypal.com>
2022-01-12 10:43:50 +0530

    Initial Commit

---
+++ SocialDownloaderApp/controllers/CategoryDownloadViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryDownloadViewController.swift
+//  SocialDownloaderApp
+//
+//  Created by Humney, Prashant on 12/01/22.
+//
+
+import UIKit
+
+class CategoryDownloadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SocialDownloaderApp/model/CategoryDownloadModel.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryDownloadModel.swift
+//  SocialDownloaderApp
+//
+//  Created by Humney, Prashant on 12/01/22.
+//
+
+
+struct CategoryDownloadModel : Codable {
+    var imageName: String
+    var socialMediaName: String
+    var totalDownloadCount: String
+}
